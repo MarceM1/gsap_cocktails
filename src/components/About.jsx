4c1d4407164cd5363f7b4280d7a3bb1079b2f273
Marcelo Melogno <marcelomelogno1986@gmail.com>
2025-06-23 19:04:42 -0300

    Modified about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -67,19 +67,49 @@ const About = () => {
 
       <div className="top-grid">
         <div className="md:col-span-3">
-            <div className="noisy"/>
+            <div className=""/>
             <img src="/images/abt1.png" alt="grid-img-1" />
         </div>
-        
+
+        <div className="md:col-span-3">
+            <div className="noisy"/>
+            <div className="flex flex-col w-full h-full px-12 py-12 text-center gap-5">
+                <h3 className="text-4xl font-modern-negra border-b-[1px] border-gray-400 pb-2 text-white">Crafted to Impress</h3>
+                <div className="flex flex-col gap-2">
+                    <div className="flex items-center gap-2">
+                        <img src="/images/check.png" alt="check" 
+                        className="size-4"
+                        />
+                        <p className="text-lg">Perfectly balanced blends</p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <img src="/images/check.png" alt="check" 
+                        className="size-4"
+                        />
+                        <p className="text-lg">Garnish to perfection</p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <img src="/images/check.png" alt="check" 
+                        className="size-4"
+                        />
+                        <p className="text-lg">Ice-cold every time</p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <img src="/images/check.png" alt="check" 
+                        className="size-4"
+                        />
+                        <p className="text-lg">Expert shaken $ stirred</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div className="md:col-span-6">
             <div className="noisy"/>
             <img src="/images/abt2.png" alt="grid-img-2" />
         </div>
         
-        <div className="md:col-span-3">
-            <div className="noisy"/>
-            <img src="/images/abt5.png" alt="grid-img-5" />
-        </div>
+        
       </div>
 
       <div className="bottom-grid">
@@ -88,7 +118,7 @@ const About = () => {
             <img src="/images/abt3.png" alt="grid-img-3" />
         </div>
         <div className="md:col-span-4">
-            <div className="noisy"/>
+            <div className=""/>
             <img src="/images/abt4.png" alt="grid-img-4" />
         </div>
       </div>
