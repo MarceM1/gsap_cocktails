43e14d08e9981a4408109df51370aa529b81fe68
Marcelo Melogno <marcelomelogno1986@gmail.com>
2025-06-23 11:12:10 -0300

    implement navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import gsap from "gsap";
+import { navLinks } from "../../constants";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    navTween.fromTo(
+      "nav",
+      {
+        backgroundColor: "transparent",
+      },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
+  return (
+    <nav>
+      <div className="">
+        <a href="#home" className="flex items-center gap-2">
+          <img src="/images/logo.png" alt="logo" />
+          <p>Velvet Pour</p>
+        </a>
+        <ul>
+          {navLinks.map(({ id, title }) => (
+            <li key={id}>
+              <a href={`#${id}`}>{title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
